d763f3deb6569b7bb6cdc232bcc7842547622136
AngelaF <ADFrys@users.noreply.github.com>
2018-04-11 23:06:00 -0500

    Update files so the bug is fixed with the title appearing on screen.

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -7,55 +7,59 @@ var axios = require("axios");
 
 
   module.exports = function(app) {
+    var finalResults = [];
 
   app.get("/", function (req, res) {
     Headline.find({}, function (error, data) {
-        if (error) {
-          console.log(error);
-        } else {
-          res.render("index", {result: data});
+      if (error) {
+        console.log(error);
+      } else {
+        res.render("index", { result: data });
+      }
+    })
+  });
+
+  app.get("/headlines", function (req, res) {
+    Headline.find({}, function (error, data) {
+      if (error) {
+        console.log(error);
+      } else {
+        res.json(data);
         }
       })
-  });
+    });
+
 
 app.get("/scrape", function(req, res) {
-  // Make a request for the news section of `ycombinator`
-  axios.get("https://www.newyorker.com/news", function(error, response, html) {
-    // Load the html body from request into cheerio
+
+  axios.get("https://www.newyorker.com/news").then(function (response) {
+
     var $ = cheerio.load(response.data);
-    // For each element with a "title" class
     $(".Card__dekImageContainer___3CRKY").each(function(i, element) {
-      // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("p")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      .children("p")
+      .text();
+      result.url = $(this)
+      .children("a")
+      .attr("href");
 
-      // Create a new Article using the `result` object built from scraping
-      Headline.create(result)
-        .then(function(dbHeadline) {
-          // View the added result in the console
-          console.log(dbHeadline);
-        })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          return res.json(err);
-        });
-        res.redirect("/")
-    });
-
-    // If we were able to successfully scrape and save an Article, send a message to the client
-    res.send("Scrape Complete");
-    });
+      finalResults.push(result);
   });
 
+    Headline.create(finalResults)
+    .then(function (dbHeadline) {
+      res.json({msg: "Scrape has completed"});
+      })
+    .catch(function (err) {
+      console.log(err);
+      })
+    });
+  }); 
 
-}
+
+};
 
 
 
